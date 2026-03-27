df1d810a0ee5564d94ebc89573ab3e5a955b70df
krishna06-git <your-github-email@example.com>
2026-03-27 19:48:39 +0530

    Fix typescript errors for deployment

---
+++ src/gsap-trial.d.ts
@@ -0,0 +1,9 @@
+declare module 'gsap-trial/SplitText' {
+  export const SplitText: any;
+}
+declare module 'gsap-trial/ScrollSmoother' {
+  export const ScrollSmoother: any;
+}
+declare module 'gsap-trial/ScrollTrigger' {
+  export const ScrollTrigger: any;
+}
